9a6f8a53ebcb1c8f056c802454e06810f2adcd58
Mykhailo <misharosa.lviv@gmail.com>
2022-03-16 12:09:17 +0200

    img to home was added

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -4,9 +4,16 @@ import "./HomePage.css"
 
 export const HomePage = () => {
     return (
-        <div className="store__list">
-            <Link className="store__item" to="/books_shop/books">Books</Link>
-            <Link className="store__item" to="/books_shop/magazines">Magazines</Link>
+        <div className="store">
+            <img
+                    className="store__img"
+                    src="https://previews.123rf.com/images/pauldesign/pauldesign1601/pauldesign160100033/51548041-welcome-we-are-open-a-welcome-sign-for-cafes-or-shop-visitors-on-blackboard-background-with-chalk-ha.jpg"
+                    alt="shop"
+            />
+            <div className="store__list">
+                <Link className="store__item" to="/books_shop/books">Books</Link>
+                <Link className="store__item" to="/books_shop/magazines">Magazines</Link>
+            </div>
         </div>
     );
 };
