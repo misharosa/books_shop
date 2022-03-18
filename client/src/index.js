6f5e839d4f940ee2b4b4082299f380fc7a227ae1
Mykhailo <misharosa.lviv@gmail.com>
2022-03-18 16:55:09 +0200

    some edit

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import { App } from './App';
 import { BrowserRouter } from "react-router-dom";
 
-const queryClient = new QueryClient();
-
 ReactDOM.render(
   <React.StrictMode>
       <BrowserRouter>
