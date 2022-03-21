cdba6927dbbb1b856434153b3f7809a937f85d02
alexbelz123 <belz.alexx@gmail.com>
2022-03-21 14:43:27 +0200

    review

--- client/src/api/api.jsx
+++ client/src/api/api.jsx
@@ -1,10 +1,14 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const getItemsFromServer = async (name) => {
-    try {
-        const { data } = await axios(`http://localhost:3001/${name}`)
-        return data
-    } catch (e) {
-        console.error(e.message)
-    }
-}
+  try {
+    const { data } = await axios(`http://localhost:3001/${name}`);
+    return data;
+  } catch (e) {
+    console.error(e.message);
+  }
+};
+
+// edit item
+
+// delete item
--- client/src/components/ItemList/ItemList.jsx
+++ client/src/components/ItemList/ItemList.jsx
@@ -1,54 +1,54 @@
 import React, { useEffect, useMemo, useState } from 'react';
-import { useParams } from "react-router-dom";
-import { getItemsFromServer} from "../../api/api";
-import { Search } from "../Search/Search";
-import { GoodItem } from "./GoodsItem/GoodItem";
-import "./ItemsList.css"
+import { useParams } from 'react-router-dom';
+import { getItemsFromServer } from '../../api/api';
+import { Search } from '../Search/Search';
+import { GoodItem } from './GoodsItem/GoodItem';
+import './ItemsList.css';
 
+// список повинен приймати те що відобразити та якийсь action(actions) який буде передаватися в ListItem на клік (в нашому випадку)
+// інша логіка повинна прийматися з пропсів
 const ItemsList = ({ items, setItems, allItemsObj, setAllItemsObj }) => {
-    const { name } = useParams()
+  const { name } = useParams();
 
-    const [filterValue, setFilterValue] = useState('')
+  const [filterValue, setFilterValue] = useState('');
 
-    useEffect(() => {
-        if(!Object.keys(allItemsObj).includes(name)) {
-            const getItems = async () => {
-                return await getItemsFromServer(name)
-            }
-            getItems().then(r => {
-                setItems(r)
-                setAllItemsObj((prev) => ({
-                        ...prev,
-                        [name]: r
-                }))
-            })
-        } else {
-            setItems(allItemsObj[name])
-        }
-    }, [setItems, name, allItemsObj, setAllItemsObj])
+  useEffect(() => {
+    if (!Object.keys(allItemsObj).includes(name)) {
+      const getItems = async () => {
+        return await getItemsFromServer(name);
+      };
+      getItems().then((r) => {
+        setItems(r);
+        setAllItemsObj((prev) => ({
+          ...prev,
+          [name]: r,
+        }));
+      });
+    } else {
+      setItems(allItemsObj[name]);
+    }
+  }, [setItems, name, allItemsObj, setAllItemsObj]);
 
-    const handleItemsFilter = useMemo(() => {
-        return items.filter(item => item.name.toLowerCase().includes(filterValue.toLowerCase()))
-    },[items, filterValue])
-
-    return (
-        <>
-            <Search
-                setFilterValue={setFilterValue}
-                filterValue={filterValue}
-            />
-            {handleItemsFilter.length > 0 &&
-                <div className='goods'>
-                    {handleItemsFilter.map(item => (
-                        <div key={item.id} className="goods__item" >
-                            <GoodItem name={name} item={item}/>
-                        </div>
-                    ))}
-                </div>
-            }
-        </>
+  const handleItemsFilter = useMemo(() => {
+    return items.filter((item) =>
+      item.name.toLowerCase().includes(filterValue.toLowerCase())
     );
-};
+  }, [items, filterValue]);
 
+  return (
+    <>
+      <Search setFilterValue={setFilterValue} filterValue={filterValue} />
+      {handleItemsFilter.length > 0 && (
+        <div className="goods">
+          {handleItemsFilter.map((item) => (
+            <div key={item.id} className="goods__item">
+              <GoodItem name={name} item={item} />
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
 
 export default ItemsList;
