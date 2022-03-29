ce56f56fc9aef6730d5605c84777c1b4c2e48714
Mykhailo <misharosa.lviv@gmail.com>
2022-03-29 09:00:51 +0300

    api was edit

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -11,31 +11,17 @@ export const getItemsFromServer = async (name) => {
 
 // edit item
 export const editItemFromServer = async (name, item) => {
-  try {
-    const { data } = await axios.patch(`http://localhost:3001/${name}/${item.id}`, item);
-    return data;
-  } catch (e) {
-    console.error(e.message);
-  }
+    return await axios.patch(`http://localhost:3001/${name}/${item.id}`, item);
 };
 
 // delete item
 export const deleteItemsFromServer = async (name, itemId) => {
-  try {
     return await axios.delete(`http://localhost:3001/${name}/${itemId}`);
-  } catch (e) {
-    console.error(e.message);
-  }
 };
 
 // add item
 
 export const addItemToServer = async (name, Item) => {
-  try {
-    const { data } = await axios.post(`http://localhost:3001/${name}`, Item);
-    return data;
-  } catch (e) {
-    console.error(e.message);
-  }
+    return await axios.post(`http://localhost:3001/${name}`, Item);
 };
 
